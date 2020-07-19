2248c5b1c58514b728408051b053d5e3a86551d3
unknown <nkumarabu@translucentcomputing.com>
2020-07-19 17:45:45 -0400

    Lab3

--- Advanced Smart Contracts/Labs/lab3/contracts/Storage.sol
+++ Advanced Smart Contracts/Labs/lab3/contracts/Storage.sol
@@ -6,9 +6,16 @@ contract Storage {
     bool    booldata = true;
     address addrdata = 0xdC962cEAb6C926E3a9B133c46c7258c0E371b82b;
 
-    function getData() public view returns (bytes4,uint64,bool,address) {
+    function getData() public view returns (bytes4 output1,uint64 output2, bool output3,address output4) {
         assembly {
             // return the values of bytes4data, uintdata, booldata, addrdata
+            let data := sload(bytes4data_slot)
+            let data2 := sload(addrdata_slot)
+            let data3 := sload(booldata_slot)
+            output1 := shl(224,and(data,0xffffffff))
+            output2 := shr(shl(3,uintdata_offset), data)
+            output3 := data3
+			 output4 := data2
         }
     }
 }
